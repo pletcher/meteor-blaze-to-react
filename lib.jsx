636394b87041579a35cad65802bff1f348cd0807
pletcher <charles.pletcher@gmail.com>
2017-07-11 15:10:18 -0400

    Use up-to-date React syntax

--- lib.jsx
+++ lib.jsx
@@ -1,33 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createContainer } from 'meteor/react-meteor-data';
 
-BlazeToReact = React.createClass({
-  mixins: [ReactMeteorData],
+class BlazeToReact extends React.Component {
   renderBlaze() {
     this.removeBlaze();
-    this.view = Blaze.renderWithData(Template[this.props.blazeTemplate], _.omit(this.props, 'blazeTemplate'), ReactDOM.findDOMNode(this.refs[this.props.blazeTemplate]));
-  },
+    this.view = Blaze.renderWithData(
+      Template[this.props.blazeTemplate],
+      _.omit(this.props, 'blazeTemplate'),
+      ReactDOM.findDOMNode(this.c)
+    );
+  }
+
   removeBlaze() {
     if (this.view) Blaze.remove(this.view);
-  },
-  getMeteorData() {
-    // Ensure a re-rendering of the template if a prop changes
-    this.renderBlaze();
-    return this.props;
-  },
+  }
+
   componentDidUpdate() {
     // Needed when used with a conditional show, like {condition ? <BlazeToCreact ../> : null}
     this.renderBlaze();
-  },
+  }
+
   componentDidMount() {
     this.renderBlaze();
-  },
+  }
+
   componentWillUnmount() {
     this.removeBlaze();
-  },
-  render() {
-    return <div ref={this.props.blazeTemplate}/>
   }
-});
 
-export default BlazeToReact;
+  render() {
+    return <div ref={c => { this.c = c; }} />
+  }
+}
+
+export default createContainer(props => props, BlazeToReact);
